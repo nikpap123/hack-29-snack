519bb78a41cd6a5a79836d6c7edbcfa34f2eb1bd
xingyaochen <xingyao314@gmail.com>
2019-07-26 11:04:19 -0700

    change pi scripts.

--- raspi-scripts/webcam.sh
+++ raspi-scripts/webcam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-DATE=$(date +"%Y-%m-%d_%H%M%S")
-fswebcam --no-banner -r 1920x1080 --crop 840x840,550x220 /home/pi/hack-29-snack/snack-photos/$DATE.jpg 
+#DATE=$(date +"%Y-%m-%d_%H%M%S")
+fswebcam --no-banner -r 1920x1080 --crop 840x840,550x220 /home/pi/hack-29-snack/devbox_backend/image.jpg 
+curl -X POST -F 'file=@/home/pi/hack-29-snack/devbox_backend/image.jpg' 172.30.166.220:5000/images
